81eff2e93eadbe9eb5d166b681e4682efc4ac46d
ec4016 <112779974+ec4016@users.noreply.github.com>
2023-10-28 19:13:03 -0400

    Create 4_Shortest_Subarray_With_Sum_At_Least_K.py

---
+++ 4_Shortest_Subarray_With_Sum_At_Least_K.py
@@ -0,0 +1,19 @@
+```py
+from collections import deque
+class Solution:
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+        dq = deque([0, 0])
+        result, curr = float('inf'), 0
+
+        for i, a in enumerate(nums):
+            curr += a
+            while dq and curr - dq[0][1] >= k:
+
+                result = min(result, i + 1 - dq.popleft()[0])
+            while dq and curr <= dq[-1][1]:
+                dq.pop()
+            dq.append([i + 1, curr])
+            if result < float('inf'):
+                return result
+            else:
+                return -1```
